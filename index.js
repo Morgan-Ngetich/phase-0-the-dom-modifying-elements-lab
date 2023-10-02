d93fe247917a112d883eb60815f553b93d496319
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-02 18:27:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+if (mainElement){
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion"
+
+
+
