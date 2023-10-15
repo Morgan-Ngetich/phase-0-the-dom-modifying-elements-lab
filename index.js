7dea5607636babba2353682079bebcae599de741
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-15 12:00:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // Write your code here!
-const mainElement = document.getElementById("main");
-if (mainElement){
-  mainElement.remove();
-}
 
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
-newHeader.textContent = "YOUR-NAME is the champion"
+const main = document.getElementById('main');
+const removeMain = main.remove()
+
+const newHeader = document.createElement('h1');
+//Create a div
+newHeader.id = 'victory'
+newHeader.textContent = 'Morgan-Ngetich is the champion'
+
+document.body.append(newHeader)
 
 
+
+
+
+
+
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
 
